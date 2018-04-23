0adaa2b8536a06d32e7839efd189ae121be931f2
Leonardo Hahn <leonardo.hahn@tfgco.com>
2018-04-23 12:11:35 -0300

    Fix test-tr_tls and make it work with cmocka.

--- test/test-tr_tls.c
+++ test/test-tr_tls.c
@@ -6,148 +6,120 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include <pomelo.h>
 #include <pomelo_trans.h>
 
-#include "test.h"
+#include "test_common.h"
 
-static pc_client_t* client;
+static pc_client_t* g_client = NULL;
 
-#define REQ_ROUTE "connector.entryHandler.entry"
-#define REQ_MSG "{\"name\": \"test\"}"
-#define REQ_EX ((void*)0x22)
-#define REQ_TIMEOUT 10
+static bool g_event_cb_called = false;
+static bool g_request_cb_called = false;
+static bool g_notify_cb_called = false;
 
-#define NOTI_ROUTE "test.testHandler.notify"
-#define NOTI_MSG "{\"content\": \"test content\"}"
-#define NOTI_EX ((void*)0x33)
-#define NOTI_TIMEOUT 30
-
-#define EV_HANDLER_EX ((void*)0x44)
-#define SERVER_PUSH "onPush"
-
-#ifdef _WIN32
-#include <windows.h>
-#define SLEEP(x) Sleep(x * 1000);
-#else
-#include <unistd.h>
-#define SLEEP(x) sleep(x)
-#endif
-
-static int local_storage_cb(pc_local_storage_op_t op, char* data, size_t* len, void* ex_data)
+static int
+setup_global_client(void **state)
 {
-    // 0 - success, -1 - fail
-    char buf[1024];
-    size_t length;
-    size_t offset;
-    FILE* f;
-
-    if (op == PC_LOCAL_STORAGE_OP_WRITE) {
-      f = fopen("pomelo.dat", "w");
-      if (!f) {
-          return -1;
-      }
-      fwrite(data, 1, *len, f);
-      fclose(f);
-      return 0;
-
-    } else {
-        f = fopen("pomelo.dat", "r");
-        if (!f) {
-            *len = 0;
-            return -1;
-        }
-        *len = 0;
-        offset = 0;
-
-        while((length = fread(buf, 1, 1024, f))) {
-            *len += length;
-            if (data) {
-                memcpy(data + offset, buf, length);
-            }
-            offset += length;
-        }
-
-        fclose(f);
-
-        return 0;
+    Unused(state);
+    // NOTE: use calloc in order to avoid one of the issues with the api.
+    // see `issues.md`.
+    g_client = calloc(1, pc_client_size());
+    if (!g_client) {
+        return -1;
     }
-}
-
-static void event_cb(pc_client_t* client, int ev_type, void* ex_data, const char* arg1, const char* arg2)
-{
-
-    PC_TEST_ASSERT(ex_data == EV_HANDLER_EX);
-    printf("test: get event %s, arg1: %s, arg2: %s\n", pc_client_ev_str(ev_type),
-            arg1 ? arg1 : "", arg2 ? arg2 : "");
-}
-
-static void request_cb(const pc_request_t* req, int rc, const char* resp)
-{
-    PC_TEST_ASSERT(rc == PC_RC_OK);
-    PC_TEST_ASSERT(resp);
-    printf("test: get resp %s\n", resp);
-    fflush(stdout);
-
-    PC_TEST_ASSERT(pc_request_client(req) == client);
-    PC_TEST_ASSERT(!strcmp(pc_request_route(req), REQ_ROUTE));
-    PC_TEST_ASSERT(!strcmp(pc_request_msg(req), REQ_MSG));
-    PC_TEST_ASSERT(pc_request_ex_data(req) == REQ_EX);
-    PC_TEST_ASSERT(pc_request_timeout(req) == REQ_TIMEOUT);
-}
-
-static void notify_cb(const pc_notify_t* noti, int rc)
-{
-    PC_TEST_ASSERT(rc == PC_RC_OK);
-
-    PC_TEST_ASSERT(pc_notify_client(noti) == client);
-    PC_TEST_ASSERT(!strcmp(pc_notify_route(noti), NOTI_ROUTE));
-    PC_TEST_ASSERT(!strcmp(pc_notify_msg(noti), NOTI_MSG));
-    PC_TEST_ASSERT(pc_notify_ex_data(noti) == NOTI_EX);
-    PC_TEST_ASSERT(pc_notify_timeout(noti) == NOTI_TIMEOUT);
-}
-
-int main()
-{
-    pc_client_config_t config = PC_CLIENT_CONFIG_DEFAULT;
-    int handler_id;
-    pc_lib_init(NULL, NULL, NULL, NULL);
-
-    config.transport_name = PC_TR_NAME_UV_TLS;
-    config.local_storage_cb = local_storage_cb;
-
-    client = (pc_client_t*)malloc(pc_client_size());
-
-    PC_TEST_ASSERT(client);
-
-    pc_client_init(client, (void*)0x11, &config);
-
-    PC_TEST_ASSERT(pc_client_ex_data(client) == (void*)0x11);
-    PC_TEST_ASSERT(pc_client_state(client) == PC_ST_INITED);
-
-    handler_id = pc_client_add_ev_handler(client, event_cb, EV_HANDLER_EX, NULL);
-
-    pc_client_connect(client, "127.0.0.1", 3011, NULL);
-    SLEEP(1);
-
-    pc_request_with_timeout(client, REQ_ROUTE, REQ_MSG, REQ_EX, REQ_TIMEOUT, request_cb);
-
-    pc_notify_with_timeout(client, NOTI_ROUTE, NOTI_MSG, NOTI_EX, NOTI_TIMEOUT, notify_cb);
-    SLEEP(50);
-
-    pc_client_disconnect(client);
-
-    pc_client_rm_ev_handler(client, handler_id);
-
-    pc_client_cleanup(client);
-
-    PC_TEST_ASSERT(pc_client_state(client) == PC_ST_NOT_INITED);
-
-    free(client);
-
-    pc_lib_cleanup();
-
     return 0;
 }
 
+static int
+teardown_global_client(void **state)
+{
+    Unused(state);
+    free(g_client);
+    g_client = NULL;
+    return 0;
+}
+
+static void
+event_cb(pc_client_t* client, int ev_type, void* ex_data, const char* arg1, const char* arg2)
+{
+    g_event_cb_called = true;
+    assert_ptr_equal(ex_data, EV_HANDLER_EX);
+//    printf("test: get event %s, arg1: %s, arg2: %s\n", pc_client_ev_str(ev_type),
+//            arg1 ? arg1 : "", arg2 ? arg2 : "");
+}
+
+static void
+request_cb(const pc_request_t* req, int rc, const char* resp)
+{
+    g_request_cb_called = true;
+    assert_int_equal(rc, PC_RC_OK);
+    assert_non_null(resp);
+//    printf("test: get resp %s\n", resp);
+//    fflush(stdout);
+
+    assert_ptr_equal(pc_request_client(req), g_client);
+    assert_string_equal(pc_request_route(req), REQ_ROUTE);
+    assert_string_equal(pc_request_msg(req), REQ_MSG);
+    assert_ptr_equal(pc_request_ex_data(req), REQ_EX);
+    assert_int_equal(pc_request_timeout(req), REQ_TIMEOUT);
+}
+
+static void
+notify_cb(const pc_notify_t* noti, int rc)
+{
+    g_notify_cb_called = true;
+    assert_int_equal(rc, PC_RC_OK);
+
+    assert_ptr_equal(pc_notify_client(noti), g_client);
+    assert_string_equal(pc_notify_route(noti), NOTI_ROUTE);
+    assert_string_equal(pc_notify_msg(noti), NOTI_MSG);
+    assert_ptr_equal(pc_notify_ex_data(noti), NOTI_EX);
+    assert_int_equal(pc_notify_timeout(noti), NOTI_TIMEOUT);
+}
+
+void
+test_tls_connection(void **state)
+{
+    Unused(state);
+    pc_client_config_t config = PC_CLIENT_CONFIG_DEFAULT;
+    config.transport_name = PC_TR_NAME_UV_TLS;
+
+    assert_non_null(g_client);
+
+#define RANDOM_PTR (void*)0xdeadbeef
+    pc_client_init(g_client, RANDOM_PTR, &config);
+
+    assert_ptr_equal(pc_client_ex_data(g_client), RANDOM_PTR);
+    assert_int_equal(pc_client_state(g_client), PC_ST_INITED);
+
+    int handler_id = pc_client_add_ev_handler(g_client, event_cb, EV_HANDLER_EX, NULL);
+
+    pc_client_connect(g_client, "127.0.0.1", TLS_PORT, NULL);
+    SLEEP_SECONDS(1);
+    pc_request_with_timeout(g_client, REQ_ROUTE, REQ_MSG, REQ_EX, REQ_TIMEOUT, request_cb);
+    pc_notify_with_timeout(g_client, NOTI_ROUTE, NOTI_MSG, NOTI_EX, NOTI_TIMEOUT, notify_cb);
+    SLEEP_SECONDS(2);
+
+    assert_true(g_event_cb_called);
+    assert_true(g_notify_cb_called);
+    assert_true(g_request_cb_called);
+
+    pc_client_disconnect(g_client);
+    pc_client_rm_ev_handler(g_client, handler_id);
+    pc_client_cleanup(g_client);
+
+    assert_int_equal(pc_client_state(g_client), PC_ST_NOT_INITED);
+
+#undef RANDOM_PTR
+}
+
+int
+main()
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test_setup_teardown(test_tls_connection, setup_global_client, teardown_global_client),
+    };
+    return cmocka_run_group_tests(tests, setup_pc_lib, teardown_pc_lib);
+}
