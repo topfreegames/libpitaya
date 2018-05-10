1f7dad41165dabd1c5db74e2f96214980ad86027
Guthyerrz Maciel <guthyerrz.ufcg@gmail.com>
2018-05-10 19:30:57 -0300

    Enabling reconnect when there is an unexpected disconnect

--- src/pc_trans.c
+++ src/pc_trans.c
@@ -128,6 +128,7 @@ void pc__trans_fire_event(pc_client_t* client, int ev_type, const char* arg1, co
 
         case PC_EV_CONNECT_ERROR:
             assert(client->state == PC_ST_CONNECTING || client->state == PC_ST_DISCONNECTING);
+            client->state = PC_ST_INITED;
             break;
 
         case PC_EV_CONNECT_FAILED:
@@ -155,6 +156,10 @@ void pc__trans_fire_event(pc_client_t* client, int ev_type, const char* arg1, co
             break;
 
         case PC_EV_UNEXPECTED_DISCONNECT:
+            assert(client->state == PC_ST_CONNECTING || client->state == PC_ST_CONNECTED
+                   || client->state == PC_ST_DISCONNECTING);
+            client->state = PC_ST_INITED;
+            break;
         case PC_EV_PROTO_ERROR:
             assert(client->state == PC_ST_CONNECTING || client->state == PC_ST_CONNECTED
                     || client->state == PC_ST_DISCONNECTING);
