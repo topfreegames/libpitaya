d6f930d93f7420f433e95c9599f1636566f239e2
Guthyerrz Maciel <guthyerrz.ufcg@gmail.com>
2018-06-14 17:37:15 -0300

    update gitignore

---
+++ build_openssl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export NDK=$1
+
+$NDK/build/tools/make_standalone_toolchain.py --api 15 --arch arm  --install-dir=`pwd`/android-toolchain-arm --deprecated-headers
+
+export TOOLCHAIN_PATH=`pwd`/android-toolchain-arm/bin
+export TOOL=arm-linux-androideabi
+export NDK_TOOLCHAIN_BASENAME=${TOOLCHAIN_PATH}/${TOOL}
+export CC=$NDK_TOOLCHAIN_BASENAME-gcc
+export CXX=$NDK_TOOLCHAIN_BASENAME-g++
+export LINK=${CXX}
+export LD=$NDK_TOOLCHAIN_BASENAME-ld
+export AR=$NDK_TOOLCHAIN_BASENAME-ar
+export RANLIB=$NDK_TOOLCHAIN_BASENAME-ranlib
+export STRIP=$NDK_TOOLCHAIN_BASENAME-strip
+export ARCH_FLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
+export ARCH_LINK="-march=armv7-a -Wl,--fix-cortex-a8"
+export CPPFLAGS=" ${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 "
+export CXXFLAGS=" ${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 -frtti -fexceptions "
+export CFLAGS=" ${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 "
+export LDFLAGS=" ${ARCH_LINK} "
+
+cd ./deps/openssl
+mkdir -p build
+
+PATH=$TOOLCHAIN_PATH:$PATH ./Configure android-armv7 --prefix=./build
+PATH=$TOOLCHAIN_PATH:$PATH make clean
+PATH=$TOOLCHAIN_PATH:$PATH make depend
+PATH=$TOOLCHAIN_PATH:$PATH make all
