bb9f082d2b5414ed0c096b95d884feac4be00990
Guthyerrz Maciel <guthyerrz.silva@tfgco.com>
2018-09-27 04:04:21 -0300

    nupackage release release

---
+++ unity/PitayaExample/Assets/Tests/JsonRequestTest.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Pitaya.Tests
+{
+    public class JsonRequestTest
+    {
+        private PitayaClient _client;
+
+#if UNITY_EDITOR
+        private const string ServerHost = "127.0.0.1";
+#else
+        private const string ServerHost = "10.0.22.57";
+#endif
+        private const int ServerPort = 3251;
+        private JObject _jsonStub;
+        private JObject _emptyJsonStub;
+        private bool _isFinished;
+        private string _data;
+        private string _emptyData;
+
+        [SetUp]
+        public void Init()
+        {
+            _client = new PitayaClient();
+            PitayaClient.SetLogLevel(PitayaLogLevel.Debug);
+            
+            _client.Connect(ServerHost, ServerPort);
+
+            _data = "{\"Data\":{\"name\":\"test25\"}}";
+            _jsonStub = (JObject)JsonConvert.DeserializeObject(_data);
+            _emptyData = "{\"Data\":{}}";
+            _emptyJsonStub = (JObject)JsonConvert.DeserializeObject(_emptyData);
+
+            _isFinished = false;
+
+            // clearing sessiondata
+            _client.Request(
+                "connector.setsessiondata",
+                _emptyJsonStub,
+                res => {},
+                error => {}
+            );
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (_client == null) return;
+            _client.Disconnect();
+            _client.Dispose();
+            _client = null;
+        }
+
+        [UnityTest]
+        public IEnumerator ShouldReturnJsonObjectWhenNoSubscriberIsAdded()
+        {
+            object response = null;
+
+            _client.Request(
+                "connector.setsessiondata",
+                _jsonStub,
+                res => {
+                    response = res;
+                    _isFinished = true;
+                },
+                error => {
+                    response = error;
+                    _isFinished = true;
+                }
+            );
+
+            while(!_isFinished) {
+                yield return new WaitForSeconds(1);
+            }
+
+            var ob = (JObject) response;
+            
+
+            Assert.True(_isFinished);
+            Assert.NotNull(response);
+            Assert.AreEqual(typeof(JObject), response.GetType());
+            Assert.AreEqual((int)ob["Code"], 200);
+            Assert.AreEqual((string)ob["Msg"], "success");
+        }
+
+        [UnityTest]
+        public IEnumerator ShouldGetAndSetJsonObject()
+        {
+            object response = null;
+
+            _client.Request(
+                "connector.setsessiondata",
+                _jsonStub,
+                res => {
+                    response = res;
+                    _isFinished = true;
+                },
+                error => {
+                    response = error;
+                    _isFinished = true;
+                }
+            );
+
+            while(!_isFinished) {
+                yield return new WaitForSeconds(1);
+            }
+
+            _isFinished = false;
+            response = null;
+
+            _client.Request(
+                "connector.getsessiondata",
+                res => {
+                    response = res;
+                    _isFinished = true;
+                },
+                error => {
+                    response = error;
+                    _isFinished = true;
+                }
+            );
+
+            while(!_isFinished) {
+                yield return new WaitForSeconds(1);
+            }
+
+            Assert.True(_isFinished);
+            Assert.NotNull(response);
+            Assert.AreEqual(response, _jsonStub);
+            Assert.AreEqual(JsonConvert.SerializeObject(response), _data);
+        }
+
+        [UnityTest]
+        public IEnumerator ShouldReceiveEmptyDataWhenSettingNullRequest()
+        {
+            object response = null;
+
+            _client.Request(
+                "connector.setsessiondata",
+                res => {
+                    response = res;
+                    _isFinished = true;
+                },
+                error => {
+                    response = error;
+                    _isFinished = true;
+                }
+            );
+
+            while(!_isFinished) {
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            _isFinished = false;
+            response = null;
+
+            _client.Request(
+                "connector.getsessiondata",
+                res => {
+                    response = res;
+                    _isFinished = true;
+                },
+                error => {
+                    response = error;
+                    _isFinished = true;
+                }
+            );
+
+            while(!_isFinished) {
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            Assert.True(_isFinished);
+            Assert.NotNull(response);
+            Assert.AreEqual(response, _emptyJsonStub);
+            Assert.AreEqual(JsonConvert.SerializeObject(response), _emptyData);
+        }
+
+        [UnityTest]
+        public IEnumerator ShouldReturnBadRequestWhenRequestingToInvalidRoute()
+        {
+            object response = null;
+
+            _client.Request(
+                "somewrongroute",
+                res => {
+                    response = res;
+                    _isFinished = true;
+                },
+                error => {
+                    response = error;
+                    _isFinished = true;
+                }
+            );
+
+            while(!_isFinished) {
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            Assert.True(_isFinished);
+            Assert.NotNull(response);
+            Assert.AreEqual(typeof(PitayaError), response.GetType());
+            Assert.AreEqual(((PitayaError)response).Code, "PIT-400");
+        }
+    }
+}
---
+++ unity/PitayaExample/Assets/Tests/JsonSerializerTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+    
+namespace Pitaya.Tests
+{
+    public class JsonSerializerTest
+    {
+
+        private JObject _jsonStub;
+        private byte[] _jsonEncoded;
+
+        [SetUp]
+        public void Init()
+        {
+            const string data = "{\"Data\":{\"name\":\"test\"}}";
+            _jsonStub = (JObject)JsonConvert.DeserializeObject(data);
+            _jsonEncoded = Encoding.UTF8.GetBytes(data);
+        }
+
+        [Test]
+        public void ShouldEncodeMessageSuccessfully()
+        {
+            var encoded = JsonSerializer.Encode(_jsonStub);
+
+            Assert.AreEqual(encoded, _jsonEncoded);
+        }
+
+        [Test]
+        public void ShouldDecodeMessageSuccessfully()
+        {
+            var decoded = JsonSerializer.Decode(_jsonEncoded);
+        
+            Assert.AreEqual(typeof(JObject), decoded.GetType());
+            Assert.AreEqual(decoded, _jsonStub);
+        }
+
+        [Test]
+        public void ShouldNotEncodeMessageIfObjectIsNotJsonObject()
+        {
+            const string wrongData = "wrong data";
+
+            Assert.Null(JsonSerializer.Encode(wrongData));
+        }
+    }
+}
