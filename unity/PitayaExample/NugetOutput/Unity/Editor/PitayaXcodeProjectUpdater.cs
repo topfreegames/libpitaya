e0b84100f79db465536c98fe4995c665dd2c4004
Leonardo Hahn <leonnardo.hahn@gmail.com>
2019-01-14 17:38:23 -0200

    Make iOS build link dynamically with zlib.
    This change fixes a crash on iOS with the `inflate` function.

---
+++ unity/PitayaExample/NugetOutput/Unity/Editor/PitayaXcodeProjectUpdater.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+
+public class PitayaBuildPostprocessor
+{
+	[PostProcessBuild]
+	public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
+	{
+		if (buildTarget == BuildTarget.iOS)
+		{
+			string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+
+			PBXProject proj = new PBXProject();
+			proj.ReadFromString(File.ReadAllText(projPath));
+
+			string target = proj.TargetGuidByName("Unity-iPhone");
+
+			// Pitaya should be linked with zlib when on iOS.
+			proj.AddBuildProperty(target, "OTHER_LDFLAGS", "-lz");
+
+			File.WriteAllText(projPath, proj.WriteToString());
+		}
+	}
+}
