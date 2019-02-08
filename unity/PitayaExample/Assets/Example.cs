e5c4da19b98f53deef8e3c364c3a0060ea946e97
Leonardo Hahn <leonnardo.hahn@gmail.com>
2019-02-08 20:23:59 -0200

    Add example scene for basic testing on mobile

---
+++ unity/PitayaExample/Assets/Example.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using UnityEngine;
+using Pitaya;
+
+public class Example : MonoBehaviour
+{
+	private PitayaClient _client;
+	private bool _connected;
+	private bool _requestSent;
+
+	// Use this for initialization
+	private void Start()
+	{
+		Debug.Log("Script loaded");
+		_client = new PitayaClient("ca.crt");
+		_connected = false;
+		_requestSent = false;
+
+		_client.NetWorkStateChangedEvent += (ev) =>
+		{
+			if (ev == PitayaNetWorkState.Connected)
+			{
+				Debug.Log("Successfully connected!");
+				_connected = true;
+			}
+		};
+
+		_client.Connect("a1d127034f31611e8858512b1bea90da-838011280.us-east-1.elb.amazonaws.com", 3252);
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		if (_connected && !_requestSent)
+		{
+			_client.Request("connector.getsessiondata",
+				(data) =>
+                {
+					Debug.Log("Got request data: " + data);
+	                File.WriteAllText("/Users/lhahn/Downloads/OH_MY_GOD.txt", "I Got the request data: " + data);
+                },
+				(err) =>
+				{
+					Debug.LogError("Got error: code = " + err.Code + ", msg = " + err.Msg);
+				});
+			_requestSent = true;
+		}
+	}
+
+	private void OnDestroy()
+	{
+		if (_client != null)
+		{
+			_client.Dispose();
+			_client = null;
+		}
+	}
+}
--- unity/PitayaExample/Assets/Pitaya/PitayaBinding.cs
+++ unity/PitayaExample/Assets/Pitaya/PitayaBinding.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Text;
 using UnityEngine;
 using System.Collections.Generic;
