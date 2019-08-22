11dc68b6712c5f65d2be655598f9535680898f7a
Rodrigo Long Chen Kashiwakura <rodrigolck@gmail.com>
2019-08-22 10:22:36 -0300

    Add UNITY_IOS compile flag

--- unity/PitayaExample/NugetOutput/Unity/Editor/PitayaXcodeProjectUpdater.cs
+++ unity/PitayaExample/NugetOutput/Unity/Editor/PitayaXcodeProjectUpdater.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEditor;
 using UnityEditor.Callbacks;
-using UnityEditor.iOS.Xcode;
 using System.IO;
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+#endif
 
 public class PitayaBuildPostprocessor
 {
 	[PostProcessBuild]
 	public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
 	{
+		#if UNITY_IOS
 		if (buildTarget == BuildTarget.iOS)
 		{
 			string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
@@ -22,5 +25,6 @@ public class PitayaBuildPostprocessor
 
 			File.WriteAllText(projPath, proj.WriteToString());
 		}
+		#endif
 	}
 }
