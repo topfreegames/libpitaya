54e50ff483f7abce6eb1cfca1a79593d72e50a96
Felippe Durán <felippe.duran@gmail.com>
2020-11-24 23:14:28 -0300

    Rename DefaultSerializerFactory to SerializerFactory and expose json serialization settings

--- unity/PitayaExample/Assets/Pitaya/ISerializerFactory.cs
+++ unity/PitayaExample/Assets/Pitaya/ISerializerFactory.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace Pitaya
 {
     public interface ISerializerFactory
@@ -6,11 +8,20 @@ namespace Pitaya
         IPitayaSerializer CreateProtobufSerializer(ProtobufSerializer.SerializationFormat format);
     }
 
-    public class DefaultSerializerFactory : ISerializerFactory
+    public class SerializerFactory : ISerializerFactory
     {
+        private readonly JsonSerializerSettings _settings;
+
+        public SerializerFactory() : this(new JsonSerializerSettings()) {}
+        
+        public SerializerFactory(JsonSerializerSettings settings)
+        {
+            _settings = settings;
+        }
+        
         public IPitayaSerializer CreateJsonSerializer()
         {
-            return new JsonSerializer();
+            return new JsonSerializer(_settings);
         }
         
         public IPitayaSerializer CreateProtobufSerializer(ProtobufSerializer.SerializationFormat format)
