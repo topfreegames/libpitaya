776f44fe96549b66fb58bc8ba967c4cd65543f53
GuilhermBrSp <guilhermbrsp@gmail.com>
2021-10-26 09:32:41 -0300

    Create pre-build and post-build script for managing binaries directories for iOS

---
+++ unity/PitayaExample/Assets/Pitaya/Editor/BuildProcessor.cs
@@ -0,0 +1,49 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class DeleteUnnecessaryResources : IPreprocessBuildWithReport, IPostprocessBuildWithReport
+{
+    private string IOS_LIB = "Assets/Pitaya/Native/iOS/";
+    
+    private const string IGNORE_MARK = "~";
+
+    private const string SIMULATOR_IDENTIFIER = "i386_x86_64";
+    
+    private const string DEVICE_IDENTIFIER = "arm64_armv7";
+ 
+    public int callbackOrder => default;
+ 
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        if (BuildTarget.iOS == EditorUserBuildSettings.activeBuildTarget)
+        {
+            string sourcePath = GetPathToHide();
+            AssetDatabase.MoveAsset(sourcePath, sourcePath + IGNORE_MARK);
+            AssetDatabase.Refresh(); 
+        }
+    }
+ 
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        if (BuildTarget.iOS == EditorUserBuildSettings.activeBuildTarget)
+        {
+            string sourcePath = GetPathToHide();
+            AssetDatabase.MoveAsset(sourcePath + IGNORE_MARK, sourcePath);
+            AssetDatabase.Refresh();
+        }
+    }
+
+    private string GetPathToHide()
+    {
+        if(PlayerSettings.iOS.sdkVersion == iOSSdkVersion.DeviceSDK)
+        {
+            return IOS_LIB + SIMULATOR_IDENTIFIER;
+        } else {
+            return IOS_LIB + DEVICE_IDENTIFIER;
+        }
+    }
+}
