f117a7c3eeac7e52a041d2fb3c6e12a05f0ab762
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 08:26:39 +0000

    Adding tarfile member sanitization to extractall()

--- build-openssl.py
+++ build-openssl.py
@@ -50,7 +50,26 @@ def make_openssl_temp_dir(openssl_tar):
         shutil.rmtree(openssl_temp_dir)
 
     with tarfile.open(openssl_tar, 'r:gz') as tar:
-        tar.extractall(tempdir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, tempdir)
 
     config_file = os.path.join(openssl_temp_dir, 'config')
     st = os.stat(config_file)
