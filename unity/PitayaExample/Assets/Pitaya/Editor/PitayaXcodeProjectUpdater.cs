17a659f3c42d9c26568125a9ee2f6ebf8004d7c5
Reinaldo Oliveira <reinaldo.oliveira@wildlifestudios.com>
2022-12-22 09:46:40 -0300

    Fix post build

--- unity/PitayaExample/Assets/Pitaya/Editor/PitayaXcodeProjectUpdater.cs
+++ unity/PitayaExample/Assets/Pitaya/Editor/PitayaXcodeProjectUpdater.cs
@@ -21,41 +21,47 @@ public class PitayaBuildPostprocessor
       CopyDirectory(dir, Path.Combine(destPath, Path.GetFileName(dir)));
   }
 
-  private const string FRAMEWORK_ORIGIN_PATH =
+  private const string FRAMEWORK_PROJECT_ORIGIN_PATH =
     "Assets/Pitaya/Native/iOS"; // relative to project folder
+  private const string FRAMEWORK_BUILD_ORIGIN_PATH =
+    "Libraries/com.wildlifestudios.nuget.libpitaya/Native/iOS"; // relative to build folder
   private const string FRAMEWORK_TARGET_PATH =
     "Frameworks"; // relative to build folder
 
   private static string[] FRAMEWORK_NAMES = {"libpitaya.xcframework", "libcrypto.xcframework", "libssl.xcframework", "libuv.xcframework"};
 
-	[PostProcessBuild]
-	public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
-	{
-		PBXProject proj = new PBXProject();
-		string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+  [PostProcessBuild]
+  public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
+  {
+    PBXProject proj = new PBXProject();
+    string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
     proj.ReadFromString(File.ReadAllText(projPath));
 
-		string targetGuid = proj.GetUnityFrameworkTargetGuid();
+    string targetGuid = proj.GetUnityFrameworkTargetGuid();
 
-		if (buildTarget == BuildTarget.iOS)
-		{
-			// Pitaya should be linked with zlib when on iOS.
-			proj.AddBuildProperty(targetGuid, "OTHER_LDFLAGS", "-lz");
+    if (buildTarget == BuildTarget.iOS)
+    {
+      // Pitaya should be linked with zlib when on iOS.
+      proj.AddBuildProperty(targetGuid, "OTHER_LDFLAGS", "-lz");
     }
-    // Add libpitaya.xcframwork
-    // 
 
+    string originPath = FRAMEWORK_PROJECT_ORIGIN_PATH;
+    if (!Directory.Exists(FRAMEWORK_PROJECT_ORIGIN_PATH)) {
+      originPath = FRAMEWORK_BUILD_ORIGIN_PATH;
+    }
+
+    // Linking frameworks
     foreach(string framework in FRAMEWORK_NAMES){
-      string sourcePath = Path.Combine(FRAMEWORK_ORIGIN_PATH, framework);
+      string sourcePath = Path.Combine(originPath, framework);
       string destPath = Path.Combine(FRAMEWORK_TARGET_PATH, framework);
-      
-      CopyDirectory(sourcePath, Path.Combine(path, destPath));
+
+      CopyDirectory(Path.Combine(path, sourcePath), Path.Combine(path, destPath));
 
       string fileGuid = proj.AddFile(destPath, destPath);
       proj.AddFileToEmbedFrameworks(targetGuid, fileGuid);
     }
     File.WriteAllText(projPath, proj.WriteToString());
-	}
+  }
 
 }
 #endif
