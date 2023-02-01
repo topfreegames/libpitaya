33cb5122ca472ce23e36c89e9dcf36b922c77f7c
Rafael da Fonseca <rafael.fonseca@wildlifestudios.com>
2023-02-01 13:41:49 +0000

    Fix directoy warnings

--- release.sh
+++ release.sh
@@ -17,4 +17,4 @@ cp libpitaya_android64/* package/Native/Android/arm64/
 cp libpitaya_android/* package/Native/Android/armv7/
 cp libpitaya_linux/* package/Native/Linux/
 cp libpitaya_mac/* package/Native/Mac/
-find libpitaya_ios -exec cp "{}" package/Native/iOS/ \;
+find libpitaya_ios -type f -exec cp "{}" package/Native/iOS/ \;
