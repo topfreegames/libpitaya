7325ac203d70cabfd483aa3b66021cedc14538e8
Rafael da Fonseca <rafael.fonseca@wildlifestudios.com>
2023-02-01 13:00:18 +0000

    diagnose artifact failure

--- release.sh
+++ release.sh
@@ -9,13 +9,12 @@ MAJOR=${BASH_REMATCH[0]}
 MINOR=${BASH_REMATCH[1]}
 PATCH=${BASH_REMATCH[2]}
 
-cp include/* nuget/Native/include
-cp -r ./unity/PitayaExample/Assets/Pitaya/Editor nuget
-sed -i "s/RELEASE-VERSION/${VERSION}/g" nuget/package.json
+cp include/* package/Native/include
+cp -r ./unity/PitayaExample/Assets/Pitaya/Editor package
+sed -i "s/RELEASE-VERSION/${VERSION}/g" package/package.json
 
-cp _builds/android64/libpitaya-android.so nuget/Native/Android/arm64/libpitaya-android.so
-cp _builds/android/libpitaya-android.so nuget/Native/Android/armv7/libpitaya-android.so
-cp _builds/linux/libpitaya-linux.so nuget/Native/Linux/libpitaya-android.so
-cp _builds/mac-xcode/Release/libpitaya-mac.bundle nuget/Native/Mac/libpitaya-mac.bundle
-cp _builds/ios/Release-iphoneos/libpitaya-ios.a nuget/Native/iOS/libpitaya-ios.a
-cp _builds/ios/deps/libuv-v1.44.2/Release-iphoneos/libuv_a.a nuget/Native/iOS/libuv_a.a
+cp libpitaya/libpitaya_android64/* package/Native/Android/arm64/
+cp libpitaya/libpitaya_android/* package/Native/Android/armv7/
+cp libpitaya/libpitaya_linux/* package/Native/Linux/
+cp libpitaya/libpitaya_mac package/Native/Mac/
+cp libpitaya/libpitaya_ios package/Native/iOS/
