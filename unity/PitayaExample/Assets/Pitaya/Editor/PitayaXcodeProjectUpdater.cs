62c5ed9d5d17cce46d0b21fa10d9283ce6aacc98
Reinaldo Oliveira <reinaldo.oliveira@wildlifestudios.com>
2023-02-07 15:10:50 -0300

    Apply changes from PR comments

--- unity/PitayaExample/Assets/Pitaya/Editor/PitayaXcodeProjectUpdater.cs
+++ unity/PitayaExample/Assets/Pitaya/Editor/PitayaXcodeProjectUpdater.cs
@@ -9,27 +9,29 @@ public class PitayaBuildPostprocessor
 	private const string SIMULATOR_IDENTIFIER = "simulator";
 	private const string DEVICE_IDENTIFIER = "device";
 	private const string FRAMEWORK_ORIGIN_PATH = "Assets/Pitaya/Native/iOS";
-	private const string FRAMEWORK_NUGET_ORIGIN_PATH = "Libraries/com.wildlifestudios.nuget.libpitaya/Native/iOS";
+	private const string FRAMEWORK_UPM_ORIGIN_PATH = "Libraries/com.wildlifestudios.nuget.libpitaya/Native/iOS";
 
 	[PostProcessBuild]
 	public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
 	{
 		if (buildTarget == BuildTarget.iOS)
 		{
-			string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+			var projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
 
 			PBXProject proj = new PBXProject();
 			proj.ReadFromString(File.ReadAllText(projPath));
 
-			string targetGuid = proj.GetUnityFrameworkTargetGuid();
+			var targetGuid = proj.GetUnityFrameworkTargetGuid();
 
 			// Pitaya should be linked with zlib when on iOS.
 			proj.AddBuildProperty(targetGuid, "OTHER_LDFLAGS", "-lz");
-
-
-			foreach (string file in Directory.GetFiles(GetPathToHide(path))) {
-				string fileGuid = proj.FindFileGuidByProjectPath(file.Substring(path.Length + 1));
-				// Files will be kept in the folder, but won't appear in the Project
+			
+			// To avoid referencing wrong libraries, the folder containing those
+			// libraries should be hidden depending on which target the build
+			// is running (device or simulator).
+			foreach (var file in Directory.GetFiles(GetPathToHide(path))) {
+				var fileGuid = proj.FindFileGuidByProjectPath(file.Substring(path.Length + 1));
+				// Files will be kept in the build folder, but won't be referenced in the built project
 				proj.RemoveFile(fileGuid);
 			}
 
@@ -39,10 +41,10 @@ public class PitayaBuildPostprocessor
 
 	private static string GetPathToHide(string projectPath)
 	{
-		string originPath = Path.Combine(projectPath, FRAMEWORK_ORIGIN_PATH);
+		var originPath = Path.Combine(projectPath, FRAMEWORK_ORIGIN_PATH);
 		if (!Directory.Exists(originPath))
 		{
-			originPath = Path.Combine(projectPath, FRAMEWORK_NUGET_ORIGIN_PATH);
+			originPath = Path.Combine(projectPath, FRAMEWORK_UPM_ORIGIN_PATH);
 		}
 
 		if(PlayerSettings.iOS.sdkVersion == iOSSdkVersion.DeviceSDK)
