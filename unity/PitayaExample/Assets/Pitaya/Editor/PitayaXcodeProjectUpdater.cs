4d94e8f8538895fcee29fd1ec0e5b4a4ca6ad452
Reinaldo Oliveira <reinaldo.oliveira@wildlifestudios.com>
2023-02-07 11:52:36 -0300

    Add post processor changes

--- unity/PitayaExample/Assets/Pitaya/Editor/PitayaXcodeProjectUpdater.cs
+++ unity/PitayaExample/Assets/Pitaya/Editor/PitayaXcodeProjectUpdater.cs
@@ -1,4 +1,4 @@
-#if UNITY_IPHONE
+#if UNITY_IOS || UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
@@ -6,6 +6,11 @@ using System.IO;
 
 public class PitayaBuildPostprocessor
 {
+	private const string SIMULATOR_IDENTIFIER = "simulator";
+	private const string DEVICE_IDENTIFIER = "device";
+	private const string FRAMEWORK_ORIGIN_PATH = "Assets/Pitaya/Native/iOS";
+	private const string FRAMEWORK_NUGET_ORIGIN_PATH = "Libraries/com.wildlifestudios.nuget.libpitaya/Native/iOS";
+
 	[PostProcessBuild]
 	public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
 	{
@@ -20,11 +25,32 @@ public class PitayaBuildPostprocessor
 
 			// Pitaya should be linked with zlib when on iOS.
 			proj.AddBuildProperty(targetGuid, "OTHER_LDFLAGS", "-lz");
-			
-			
-			
+
+
+			foreach (string file in Directory.GetFiles(GetPathToHide(path))) {
+				string fileGuid = proj.FindFileGuidByProjectPath(file.Substring(path.Length + 1));
+				// Files will be kept in the folder, but won't appear in the Project
+				proj.RemoveFile(fileGuid);
+			}
+
 			File.WriteAllText(projPath, proj.WriteToString());
 		}
+	}
+
+	private static string GetPathToHide(string projectPath)
+	{
+		string originPath = Path.Combine(projectPath, FRAMEWORK_ORIGIN_PATH);
+		if (!Directory.Exists(originPath))
+		{
+			originPath = Path.Combine(projectPath, FRAMEWORK_NUGET_ORIGIN_PATH);
+		}
+
+		if(PlayerSettings.iOS.sdkVersion == iOSSdkVersion.DeviceSDK)
+		{
+			return Path.Combine(originPath, SIMULATOR_IDENTIFIER);
+		}
+
+		return Path.Combine(originPath, DEVICE_IDENTIFIER);
 	}
 }
 #endif
