806c541c70fb23f88108fa18ce1cf9b1a04b647b
Reinaldo Oliveira <reinaldo.oliveira@wildlifestudios.com>
2023-05-08 15:33:22 -0300

    Fix package script

--- package.sh
+++ package.sh
@@ -10,22 +10,22 @@ MINOR=${BASH_REMATCH[2]}
 PATCH=${BASH_REMATCH[3]}
 SUFFIX=${BASH_REMATCH[4]}
 
-sed -i "s/PC_VERSION_MAJOR.*/PC_VERSION_MAJOR $MAJOR/g" include/pitaya_version.h 
-sed -i "s/PC_VERSION_MINOR.*/PC_VERSION_MINOR $MINOR/g" include/pitaya_version.h 
+sed -i "s/PC_VERSION_MAJOR.*/PC_VERSION_MAJOR $MAJOR/g" include/pitaya_version.h
+sed -i "s/PC_VERSION_MINOR.*/PC_VERSION_MINOR $MINOR/g" include/pitaya_version.h
 sed -i "s/PC_VERSION_REVISION.*/PC_VERSION_REVISION $PATCH/g" include/pitaya_version.h
 sed -i "s/PC_VERSION_SUFFIX.*/PC_VERSION_SUFFIX \"${SUFFIX}\"/g" include/pitaya_version.h
 
 sed -i "s/RELEASE-VERSION/${VERSION}/g" package/package.json
-sed -i "s/RELEASE-VERSION/${VERSION}/g" unity/PitayaExample/LibPitaya.nuspec 
+sed -i "s/RELEASE-VERSION/${VERSION}/g" unity/PitayaExample/LibPitaya.nuspec
 
-cp include/* package/Native/include
+cp include/* package/PitayaNativeLibraries/include
 cp -r ./unity/PitayaExample/Assets/Pitaya/Editor package
 
-cp libpitaya_android64/* package/Native/Android/arm64/
-cp libpitaya_android/* package/Native/Android/armv7/
-cp libpitaya_linux/* package/Native/Linux/
-cp libpitaya_mac/* package/Native/Mac/
-find libpitaya_ios -type f -exec cp "{}" package/Native/iOS/device/ \;
-find libpitaya_ios-simulator -type f -exec cp "{}" package/Native/iOS/simulator/ \;
+cp libpitaya_android64/* package/PitayaNativeLibraries/Android/arm64/
+cp libpitaya_android/* package/PitayaNativeLibraries/Android/armv7/
+cp libpitaya_linux/* package/PitayaNativeLibraries/Linux/
+cp libpitaya_mac/* package/PitayaNativeLibraries/Mac/
+find libpitaya_ios -type f -exec cp "{}" package/PitayaNativeLibraries/iOS/device/ \;
+find libpitaya_ios-simulator -type f -exec cp "{}" package/PitayaNativeLibraries/iOS/simulator/ \;
 
 cp -r pitaya-dlls/* package/
